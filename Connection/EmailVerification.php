1476bae6386913d6c5318bc760a512c289a69547
Ken Santos <kenxsantos@gmail.com>
2025-03-23 21:50:45 +0800

    feat: implement email verification functionality with PHPMailer integration

---
+++ Connection/EmailVerification.php
@@ -0,0 +1,18 @@
+<?php
+
+define('MAILHOST', 'smtp.gmail.com');
+
+//create new email for quatropasos. Like quatropasos@gmail.com
+define('USERNAME', 'kenxsantos@gmail.com');
+
+//Allow mo 2 factor authentication sa gmail mo. Then create app password.
+//Then use that app password here.
+define('PASSWORD', 'ruhz bnvb glfg ujac');
+
+define('SEND_FROM', 'quatropasos@gmail.com');
+
+define('SEND_FROM_NAME', 'Quatro Pasos');
+
+define('REPLY_TO', 'quatropasos.inquiry@gmail.com');
+
+define('REPLY_TO_NAME', 'Quatro Pasos Inquiry');
